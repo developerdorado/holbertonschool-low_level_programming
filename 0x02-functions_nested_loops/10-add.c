1f9791a4089826d881ad0e589591ed0f4301f192
developerdorado <rafaeldorado001@gmail.com>
2021-02-09 09:13:46 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - unction that adds two integers.
+ * @n1: number one
+ * @n2: number two
+ * Return: success (0)
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
