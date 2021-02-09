d085961bbc6e6e81a080e9f49d0f23d8fc518004
developerdorado <rafaeldorado001@gmail.com>
2021-02-09 05:33:35 +0000

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  * print_alphabet - Print the alphabet
+  * Return: success (0)
+  */
+
+ void print_alphabet(void)
+{
+	char i = 'a';
+	while(i <= 'z' )
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
