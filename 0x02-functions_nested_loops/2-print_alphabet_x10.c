f2efe357406045764777da2ada6057c92524d926
developerdorado <rafaeldorado001@gmail.com>
2021-02-09 02:06:59 +0000

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* print_alphabet_x10 - Print the alphabet x 10
+* Return: success (0)
+*/
+
+void print_alphabet_x10(void)
+{
+char i = 'a';
+int x = 0;
+for(x; x <= 10; x++)
+{
+for(; i <= 'z'; i++)
+{
+_putchar(i);
+}
+i = 'a';
+_putchar('\n');
+}
+}
