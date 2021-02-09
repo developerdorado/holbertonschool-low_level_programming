ee5703ca3b8dfb0e8d9d792dabfe1386ca947159
developerdorado <rafaeldorado001@gmail.com>
2021-02-09 06:36:02 +0000

    7. There are only 3 colors, 10 digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,7 @@ int print_last_digit(int n)
 	int l = n % 10;
 
 	if (l < 0)
-	{
-		_putchar((l * -1) + '0');
-	}
-	else
-	{
-		_putchar(l + '0');
-	}
+		l = l * -1;
+	_putchar(l + '0');
 	return (0);
 }
