a1cc555dbf7612817f2568a510bd12f4b5684508
developerdorado <rafaeldorado001@gmail.com>
2021-02-17 19:44:16 +0000

    Atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _atoi - Function that convert a string to an integer.
+ * @s: String
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+	unsigned int i, n;
+	int x;
+
+	n = 0;
+	x = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = s[i] - '0' + n * 10;
+			if (s[i + 1] == ' ')
+			{
+				break;
+			}
+			else if
+				(s[i] == '-')
+				{
+					x = x * -1;
+				}
+		}
+	}
+	return (n * x);
+}
